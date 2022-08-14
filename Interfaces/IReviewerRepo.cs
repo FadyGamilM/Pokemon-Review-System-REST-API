9b9efc471ce060bf53d1afb2510068410d08e35b
fady <gamilfady605@gmail.com>
2022-08-14 22:02:09 +0200

    Reviewer Service [interface + repository] is done

---
+++ Interfaces/IReviewerRepo.cs
@@ -0,0 +1,16 @@
+using pokemonAPI.Models;
+namespace pokemonAPI.Interfaces
+{
+   public interface IReviewerRepo
+   {
+
+      // get all reviewers 
+      Task<IEnumerable<Reviewer>> GetReviewers();
+      // get reviewer by id 
+      Task<Reviewer> GetReveiewer(int reviewerID);
+      // get all reviews of specific reviewer 
+      Task<IEnumerable<Review>> GetReviews(int reviewerID);
+      // get all the pokemons that this reviewer wrote reviews about
+      Task<IEnumerable<Pokemon>> GetPokemons(int reviewerID);
+   }
+}
---
+++ Repositories/ReviewerRepo.cs
@@ -0,0 +1,48 @@
+using Microsoft.EntityFrameworkCore;
+using pokemonAPI.Data;
+using pokemonAPI.Interfaces;
+using pokemonAPI.Models;
+
+namespace pokemonAPI.Repositories
+{
+   public class ReviewerRepo : IReviewerRepo
+   {
+      // DI pattern
+      private readonly AppDbContext _context;
+      public ReviewerRepo(AppDbContext context)
+      {
+         this._context = context;
+      }
+      // get all reviewers 
+      public async Task<IEnumerable<Reviewer>> GetReviewers()
+      {
+         var reviewers = await this._context.Reviewers.AsNoTracking().ToListAsync();
+         return reviewers;
+      }
+      // get reviewer by id 
+      public async Task<Reviewer> GetReveiewer(int reviewerID)
+      {
+         var reviewer = await this._context.Reviewers.FindAsync(reviewerID);
+         return reviewer;
+      }
+      // get all reviews of specific reviewer 
+      public async Task<IEnumerable<Review>> GetReviews(int reviewerID)
+      {
+         var reviewer = await this._context.Reviewers.FindAsync(reviewerID);
+         await this._context.Entry(reviewer).Collection(R => R.Reviews).LoadAsync();
+         return reviewer.Reviews;
+      }
+      // get all the pokemons that this reviewer wrote reviews about
+      public async Task<IEnumerable<Pokemon>> GetPokemons(int reviewerID)
+      {
+         var reviewer = await this._context.Reviewers.FindAsync(reviewerID);
+         await this._context.Entry(reviewer).Collection(R => R.Reviews).LoadAsync();
+         var pokemons = new List<Pokemon>();
+         foreach (var review in reviewer.Reviews)
+         {
+            pokemons.Add(review.Pokemon);
+         }
+         return pokemons;
+      }
+   }
+}
